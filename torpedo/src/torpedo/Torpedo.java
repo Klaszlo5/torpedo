e959bb9864e3e6795312060fa79974c58170cb63
Klaszlo5 <94529898+Klaszlo5@users.noreply.github.com>
2022-05-16 13:48:21 +0200

    Update Torpedo.java

--- torpedo/src/torpedo/Torpedo.java
+++ torpedo/src/torpedo/Torpedo.java
@@ -1,9 +1,25 @@
 package torpedo;
 
 public class Torpedo {
-
     public static void main(String[] args) {
-       
+       int[][] palya = new int[1][7];
+        int[][] hajok = new int[5][3];
     }
-    
+    public static void Palya1(int[][] palya){
+        for(int index=0 ; index < 5 ; index++ )
+            for(int sor=0 ; sor < 5 ; sor++ )
+                palya[index][sor]=-1;
+    }
+    public static void PalyaKiir(int[][] palya){   
+    for(int sorb=0 ; sorb < 5 ; sorb++ ){
+        System.out.print((sorb+1)+"");
+        for(int sora=0 ; sora < 5 ; sora++ ){
+            if(palya[sorb][sora]==-1){
+                System.out.print("\t"+"~");
+            }else if(palya[sorb][sora]==0){
+                System.out.print("\t"+"*");
+            }
+        }
+    }
+}
 }
